6b3402f0a0d7ad75059d78fc2e2eb87793f9ac5e
NestBris <63145839+sitechbr@users.noreply.github.com>
2023-12-21 16:41:55 +0300

    Update pandas_excel_duplicates.py

--- pandas_excel_duplicates.py
+++ pandas_excel_duplicates.py
@@ -1,9 +1,10 @@
 import pandas as pd
+from pathlib import Path
 
 gophish = "gophish_import.csv"
 
-report1c = pd.read_excel('report_1c.xlsx')
-vip = pd.read_excel('vip.xlsx')
+report1c = pd.read_excel('report_1c.xlsx',dtype=str)
+vip = pd.read_excel('vip.xlsx',dtype=str)
 report1c_df = pd.DataFrame(report1c, columns=['Организация','Сотрудник', 'Адрес электронной почты', 'Должность'])
 report1c_df.columns=['First Name','Last Name','Email','Position']
 
@@ -20,4 +21,6 @@ foo_df = report1c_df.drop_duplicates(subset=['Email'])
 #gophish_df = pd.concat([foo_df,vip_df]).drop_duplicates(subset=['Last Name','Position'],keep=False).dropna(subset=['Email'])
 #Удаление випов по списку
 gophish_df = foo_df[~foo_df['Last Name'].isin(vip_list)]
-gophish_df.to_csv(gophish,sep=',',encoding='utf-8',index=False,header=False)
+gophish_df.to_csv(gophish,sep=',',encoding='utf-8',index=False)
+
+print(str(Path.cwd())+'/'+gophish)
