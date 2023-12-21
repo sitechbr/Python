8bee5673f8924d9c207a522e50baf5d5f2d077b5
NestBris <63145839+sitechbr@users.noreply.github.com>
2023-12-21 14:38:43 +0300

    Update pandas_excel_duplicates.py

--- pandas_excel_duplicates.py
+++ pandas_excel_duplicates.py
@@ -4,14 +4,20 @@ gophish = "gophish_import.csv"
 
 report1c = pd.read_excel('report_1c.xlsx')
 vip = pd.read_excel('vip.xlsx')
-report1c_df = pd.DataFrame(report1c, columns=['Name', 'Email', 'Позиция'])
-vip_df = pd.DataFrame(vip,columns=['Name','Позиция'])
+report1c_df = pd.DataFrame(report1c, columns=['Организация','Сотрудник', 'Адрес электронной почты', 'Должность'])
+report1c_df.columns=['First Name','Last Name','Email','Position']
+
+vip_df = pd.DataFrame(vip,columns=['ФИО','Должность'])
+vip_df.columns=['Last Name','Position']
+vip_list = vip_df['Last Name'].unique()
+
+
 
 #Удаление дупликатов
 foo_df = report1c_df.drop_duplicates(subset=['Email'])
+
 #Добавление в таблицу випов и удаление дубликатов полностью
-gophish_df = pd.concat([foo_df,vip_df]).drop_duplicates(subset=['Name','Позиция'],keep=False)
-gophish_df.to_csv('gophish_import.csv',sep=',',encoding='utf-8',index=False,header=False)
-
-
-
+#gophish_df = pd.concat([foo_df,vip_df]).drop_duplicates(subset=['Last Name','Position'],keep=False).dropna(subset=['Email'])
+#Удаление випов по списку
+gophish_df = foo_df[~foo_df['Last Name'].isin(vip_list)]
+gophish_df.to_csv(gophish,sep=',',encoding='utf-8',index=False,header=False)
